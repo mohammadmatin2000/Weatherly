242d7e25673fe70c3831655382b9e4725957d23b
MATIN <mohammadmatin13872008@gmail.com>
2025-09-24 19:34:39 +0330

    Create Url

--- core/core/settings.py
+++ core/core/settings.py
@@ -123,6 +123,6 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-  "http://localhost:3000",  
+  "http://localhost:3000",
 ]
 CORS_ALLOW_ALL_ORIGINS = True
